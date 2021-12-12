3267b5533173f540709f353db9a3ce61798a1d91
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-12-12 09:34:51 +0545

    Updates

---
+++ precache-manifest.97d935edc8768d342bc99e5ae0cc511d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593d7d4a1a40acbc5681c1920615525f",
+    "url": "/react_pwa_plus_deployment/index.html"
+  },
+  {
+    "revision": "cf6caad91c1533258459",
+    "url": "/react_pwa_plus_deployment/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "c81d3e8ff22540c9298e",
+    "url": "/react_pwa_plus_deployment/static/css/main.f4502ddc.chunk.css"
+  },
+  {
+    "revision": "cf6caad91c1533258459",
+    "url": "/react_pwa_plus_deployment/static/js/2.bbd893a5.chunk.js"
+  },
+  {
+    "revision": "b9981179dddf661bda491c86af116c02",
+    "url": "/react_pwa_plus_deployment/static/js/2.bbd893a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68762a85be753475d4a",
+    "url": "/react_pwa_plus_deployment/static/js/3.ebe430f7.chunk.js"
+  },
+  {
+    "revision": "c81d3e8ff22540c9298e",
+    "url": "/react_pwa_plus_deployment/static/js/main.94880587.chunk.js"
+  },
+  {
+    "revision": "8c0cd3bc89f7cbfbae94",
+    "url": "/react_pwa_plus_deployment/static/js/runtime-main.ce3c1af5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react_pwa_plus_deployment/static/media/SEGA.12a3db8b.woff"
+  }
+]);
